e3817763bbddd7c1d2764ec079bfd40c64bce86d
Jenna Quint <jennaquint1@gmail.com>
2014-01-29 15:23:44 -0500

    Getting http://loaclhost:3000/pictures to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = '01bc3a756ee6371bdf02ce4763d2b3ad399be3576bd6c06b69d4213ceefb2a9f91b12770b8867f8a76b6ec54b48c64feec0bdfb7bb5a768f9e42fa6bb0a5f691'
